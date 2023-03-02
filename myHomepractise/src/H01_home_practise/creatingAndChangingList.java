da51b398c6a8357004f33767066a6e2ff316c591
Berke <93223660+quatronostro@users.noreply.github.com>
2023-03-02 13:01:26 +0300

    Update creatingAndChangingList.java

--- myHomepractise/src/H01_home_practise/creatingAndChangingList.java
+++ myHomepractise/src/H01_home_practise/creatingAndChangingList.java
@@ -7,14 +7,14 @@ public class odev01 {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+        
         int getIndex;
         int setIndex;
         String yesOrNo = "";
-        boolean flag = false;
+        
+        List<Integer> lastVersion = new ArrayList<>(creatingList());
 
-        List<Integer> sonHali = new ArrayList<>(creatingList());
-
-        System.out.println(sonHali);
+        System.out.println(lastVersion);
 
         System.out.println("Do you want to change elements in your list? " +
                 "\nIf your answer yes, please press 'Y'" +
@@ -26,10 +26,10 @@ public class odev01 {
             getIndex = scan.nextInt();
             System.out.println("Please enter your new element you want to enter : ");
             setIndex = scan.nextInt();
-            System.out.println(changeListElement(sonHali, getIndex, setIndex));
+            System.out.println(changeListElement(lastVersion, getIndex, setIndex));
 
         } else if (Pattern.matches("[nN]", yesOrNo)) {
-            System.out.println(sonHali);
+            System.out.println(lastVersion);
         }
 
 
